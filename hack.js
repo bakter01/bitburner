cf596a7ffbaebcc41ea2e4a38858d6f52fe830a1
Bakter01 <uberwitcher@gmail.com>
2026-03-19 11:51:51 +0100

    hack.js: Optimize hack strategy

--- hack.js
+++ hack.js
@@ -1,19 +1,22 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    const target = ns.args[0]; // Target settings
-    const moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    const target = ns.args[0];
+    
+    // Határértékek: a szerver alapértékeihez képest számolunk
+    const moneyThresh = ns.getServerMaxMoney(target) * 0.9;
     const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
 
-    // Infinite loop
-    while (true) {
+    while(true) {
+        // Ha a biztonság elszállt, ELŐSZÖR gyengítünk
         if (ns.getServerSecurityLevel(target) > securityThresh) {
-            // If security is high weaken it
             await ns.weaken(target);
-        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-            // If money is low raise it
+        } 
+        // Ha a pénz kevés, növelünk
+        else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
             await ns.grow(target);
-        } else {
-            // If optimal hack
+        } 
+        // Csak ha minden ideális, akkor hackelünk
+        else {
             await ns.hack(target);
         }
     }
